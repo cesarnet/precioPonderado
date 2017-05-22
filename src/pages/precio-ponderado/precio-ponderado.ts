216ada265164f176a138926a1779d1029f85db31
Cesarnet <cesar.sandin@gmail.com>
2017-05-22 15:42:54 +0000

    Second commit

---
+++ src/pages/precio-ponderado/precio-ponderado.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-precio-ponderado',
+  templateUrl: 'precio-ponderado.html'
+})
+export class PrecioPonderadoPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
